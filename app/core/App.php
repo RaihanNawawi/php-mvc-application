4ebd9cdcbe9f93790c84812fbb2838777cdff59a
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-11 15:07:07 +0700

    Feat: Menangkap data yang dikirimkan di url dan menampilkannya ke layar

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,8 @@
 
 class App {
     public function __construct() {
-        echo "App class has been initialized. <hr>";
+        // Mengecek apapun yang dikirim melalui URL
+        // Misal http://localhost/phpmvc/public/?url=inidariuser
+       var_dump($_GET); // akan menampilkan array dengan key url dan value "inidariuser"
     }
 }
