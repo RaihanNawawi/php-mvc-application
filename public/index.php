82aeea90667db2a5c2534d5fb94e15ab30023619
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-12 13:52:51 +0700

    Feat: Studi kasus menggunakan htaccess logic untuk membuat pretty URL

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My MVC App</title>
+</head>
+<body>
+    <h1>Welcome to My MVC Application</h1>
+    <?php
 
 require_once '../app/init.php';
 
 $app = new App();
 $controller = new Controller();
+?>
+</body>
+</html>
