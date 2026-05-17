5ad08b48c477a9d15796735810c647b058b68edb
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-17 11:15:47 +0700

    Feat: Membuat logic untuk memanggil params yang ditulis user di url dan logic untuk menjalankan semua controller, method dan parameter yang dikirimkan oleh user di url

--- app/core/App.php
+++ app/core/App.php
@@ -15,13 +15,21 @@ class App {
         require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        // Jika ada method yang dipanggil melalui url, maka method tersebut akan dijalankan
+        // Method Logic
         if ( isset($url[1]) ) {
             if ( method_exists($this->controller, $url[1]) ) {
                 $this->method = $url[1];
                 unset($url[1]);
             }
         }
+
+        // Params Logic
+        if ( !empty($url) ) {
+            $this->params = array_values($url);
+        }
+
+        // jalankan controller dan method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseURL() {
