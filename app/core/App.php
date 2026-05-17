18edad06226e337fcd65e8a92ac4efc4fc011504
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-17 11:03:38 +0700

    Feat: Membuat logic untuk memanggil method yang ditulis user di url

--- app/core/App.php
+++ app/core/App.php
@@ -7,13 +7,21 @@ class App {
 
     public function __construct() {
         $url = $this->parseURL();
-        // cek apakah file controller yang dipanggil ada di folder controllers, jika ada maka controller akan diubah sesuai dengan url yang dipanggil
+        // Controller Logic
         if ( file_exists('../app/controllers/' . $url[0] . '.php') ) {
             $this->controller = $url[0];
             unset($url[0]);
         }
         require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
+
+        // Jika ada method yang dipanggil melalui url, maka method tersebut akan dijalankan
+        if ( isset($url[1]) ) {
+            if ( method_exists($this->controller, $url[1]) ) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
     }
 
     public function parseURL() {
