d1069c58c90420d68694cadbc21d54de56cbe8e3
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-25 11:01:38 +0700

    Feat: Menggunakan PDO option untuk mengatur perilaku/behavior koneksi PDO ke database yang dapat meningkatkan performa ke database (Flow #2)

--- app/core/Database.php
+++ app/core/Database.php
@@ -15,9 +15,15 @@ class Database {
         // data source name
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db;
 
+        // Optimasi koneksi ke database dengan persistent connection dan error mode exception
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
         // cek apakah koneksi berhasil
         try {
-            $this->dbh = new PDO($dsn, $this->user, $this->pass);
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
         } catch (PDOException $e) {
             die($e->getMessage());
         }
