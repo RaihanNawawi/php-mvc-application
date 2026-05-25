e670db137088317de469bca83f817a91ba451af9
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-25 11:09:28 +0700

    Feat: Menggunakan workflow Prepared Statement PDO agar query yang dikirim ke database aman dari sql injection (Flow #3)

--- app/core/Database.php
+++ app/core/Database.php
@@ -28,4 +28,38 @@ class Database {
             die($e->getMessage());
         }
     }
+
+       // Prepare statement dengan query
+    public function query($query)
+    {
+        $this->stmt = $this->dbh->prepare($query);
+    }
+
+    // Bind nilai ke statement
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    // Eksekusi statement
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
 }
