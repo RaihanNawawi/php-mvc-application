ca4edbfd5c014cab0ae557a1a91d63653937256e
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-05-26 10:30:00 +0700

    Feat: Studi kasus menampilkan semua data dari tabel mahasiswa dengan menggunakan database wrapper (Flow #6)

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,26 +2,17 @@
 
 class Mahasiswa_model
 {
-    private $dbh; // database handler
-    private $stmt; // statement (query)
+    private $table = 'mahasiswa'; // Nama tabel di database
+    private $dbh; // Database handler/koneksi ke database
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        // cek apakah koneksi berhasil
-        try {
-            $this->dbh = new PDO($dsn, 'root', ''); // PDO diisi dengan $dsn, username ('root'), password (''). 
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->dbh = new Database(); // Membuat instance dari class Database untuk koneksi ke database
     }
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare("SELECT * FROM mahasiswa");
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->dbh->query('SELECT * FROM ' . $this->table); // Menyiapkan query untuk mengambil semua data mahasiswa
+        return $this->dbh->resultSet();
     }
 }
