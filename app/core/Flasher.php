9690ebcd81ec9427f25901205505dfd7b3fc9adf
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-06-01 09:34:37 +0700

    Feat: Membuat static method untuk tampilan/format yang akan digunakan oleh flash message yang disesuaikan dengan data yang ditangkap (Template flash message yang datanya diambil dari session) (Flow #2)

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -12,4 +12,16 @@ class Flasher {
             'tipe' => $tipe
         ];
     }
+
+    // Menampilkan pesan flash jika terdapat pesan dalam session (terisi/isset), dan kemudian menghapusnya dari session
+    public static function flash()
+    {
+        if (isset($_SESSION['flash'])) {
+            echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
+                    ' . $_SESSION['flash']['status'] . ' ' . $_SESSION['flash']['aksi'] . '
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            unset($_SESSION['flash']);
+        }
+    }
 }
