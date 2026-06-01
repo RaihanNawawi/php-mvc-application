bdfc0581ae96c067a2883151fa4f5362dd701af2
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-06-01 11:08:17 +0700

    Style: Memperbaiki tampilan format dari flash message (Flow #5)

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -18,7 +18,7 @@ class Flasher {
     {
         if (isset($_SESSION['flash'])) {
             echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
-                    ' . $_SESSION['flash']['status'] . ' ' . $_SESSION['flash']['aksi'] . '
+                    Data Mahasiswa <b>' . ucfirst($_SESSION['flash']['status']) . '</b> ' . $_SESSION['flash']['aksi'] . '!
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>';
             unset($_SESSION['flash']);
